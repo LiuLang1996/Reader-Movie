a305fb6a7a9f1404fbc8445ad10faa48605447bf
Allen <809721414@qq.com>
2017-10-16 20:37:24 +0800

    添加图片资源和文章列表页面

---
+++ pages/posts/post.js
@@ -0,0 +1,78 @@
+import { postList } from '../../data/posts-data.js';
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      '/images/iqiyi.png',
+      '/images/vr.png',
+      '/images/wx.png'
+    ],
+    autoplay: true,
+    interval: 5000,
+    circular: true,
+    indicatorDots: true
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      postLists: postList
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/welcome/welcome.js
@@ -0,0 +1,10 @@
+Page({
+  /**
+   * 重定向路由
+   */
+  onTap: function () {
+    wx.redirectTo({
+      url: '/pages/posts/post',
+    })
+  }
+})
